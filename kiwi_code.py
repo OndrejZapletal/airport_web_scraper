0f4138e4af9dc53776456519a03cd9f810114e4b
Ondrej Zapletal <ondrej.matej.zapletal@gmail.com>
2017-03-01 14:54:59 +0100

    adding threadpool and removng single thread option

--- kiwi_code.py
+++ kiwi_code.py
@@ -5,73 +5,69 @@
 from threading import Thread, RLock
 from queue import Queue
 import csv
-import sys
 import re
 from urllib.request import Request, urlopen, URLError
+from time import sleep
 from bs4 import BeautifulSoup
 
 ADDRESS = "https://www.world-airport-codes.com/"
 COUNTRY_RE = re.compile(r".+\((\w\w)\).*")
 
 
-def compose_request(airport_key):
-    """compose string for search request"""
-    if len(airport_key) != 3:
-        raise Exception("Airport key '%s' is not valid" % airport_key)
-    request = "%ssearch/?s=%s" % (ADDRESS, airport_key.lower())
-    return request
+class Worker(Thread):
+    """ Thread executing tasks from a given tasks queue """
+    def __init__(self, tasks):
+        Thread.__init__(self)
+        self.tasks = tasks
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        while True:
+            func, args, kargs = self.tasks.get()
+            try:
+                func(*args, **kargs)
+            except Exception as error:
+                # An exception happened in this thread
+                print(error)
+            finally:
+                # Mark this task as done, whether an exception happened or not
+                self.tasks.task_done()
 
 
-def get_airport_country(airport_key):
-    """Fetch airport info from www.world-airport-codes.com."""
-    try:
-        req = Request(compose_request(airport_key), headers={'User-Agent' : "kiwi project"})
-        response = urlopen(req)
-        country_code = parse_country_code(response.read())
-        if country_code:
-            with lock:
-                dictionary_of_airports[airport_key] = country_code
-        else:
-            print("Counld not find country for '%s' airport" % airport_key)
-    except URLError as error:
-        print("Error: %s" % error)
+class ThreadPool:
+    """ Pool of threads consuming tasks from a queue """
+    def __init__(self, num_threads):
+        self.tasks = Queue(num_threads)
+        for _ in range(num_threads):
+            Worker(self.tasks)
+
+    def add_task(self, func, *args, **kargs):
+        """ Add a task to the queue """
+        self.tasks.put((func, args, kargs))
+
+    def map(self, func, args_list):
+        """ Add a list of tasks to the queue """
+        for args in args_list:
+            self.add_task(func, args)
+
+    def wait_completion(self):
+        """ Wait for completion of all the tasks in the queue """
+        self.tasks.join()
 
 
+def main(size_of_pool):
+    """main function"""
+    airports = get_list_of_airports()
+    print("Gathering information about %s airports" % len(airports))
+    get_dictionary_of_airports(airports, size_of_pool)
 
-def parse_country_code(response):
-    """returns code of country"""
-    soup = BeautifulSoup(response, "html.parser")
-    result = str(soup.find_all("div", "header clearfix")[0])
-    soup = BeautifulSoup(result, "html.parser")
-    result = str(soup.find("p").contents[0][1:-1])
-    try:
-        code = COUNTRY_RE.match(result).group(1)
-    except AttributeError:
-        print("result: %s" % result)
-        code = None
-    return code
+    airport_list_text = ""
+    for airport, country in dictionary_of_airports.items():
+        airport_list_text += "%s:%s\n" % (airport, country)
 
-
-def get_dictionary_of_airports(list_of_airports, multithreded):
-    """Creates dictionary containg 'airport code' : 'country' pairs."""
-    if multithreded:
-        print("Starting multithreaded version")
-        threads = []
-
-        for airport in list_of_airports:
-            threads.append(Thread(target=get_airport_country, args=(airport,)))
-
-        for thread in threads:
-            thread.daemon = True
-            thread.start()
-
-        for thread in threads:
-            thread.join()
-    else:
-        print("Starting single thread version")
-        for airport in list_of_airports:
-            get_airport_country(airport)
-
+    with open("airport_names.txt", "w") as airports_file:
+        airports_file.write(airport_list_text)
 
 
 def get_list_of_airports():
@@ -84,34 +80,72 @@ def get_list_of_airports():
         for row in flights_reader:
             if row[0] not in list_of_airports:
                 list_of_airports.append(row[0])
-
-            if row[1] not in list_of_airports:
-                list_of_airports.append(row[1])
     return list_of_airports
 
 
-def main():
-    """main function"""
-    multithreaded = True
-    if len(sys.argv) > 1:
-        if sys.argv[1] == "1":
-            multithreaded = False
+def get_dictionary_of_airports(list_of_airports, size_of_pool):
+    """Creates dictionary containing 'airport code' : 'country' pairs."""
+    thread_pool = ThreadPool(size_of_pool)
+    thread_pool.map(get_airport_country, list_of_airports)
+    thread_pool.wait_completion()
 
-    airports = get_list_of_airports()
-    print("Gathering information about %s airports" % len(airports))
-    get_dictionary_of_airports(airports, multithreaded)
 
-    airport_list_text = ""
-    for airport, country in dictionary_of_airports.items():
-        airport_list_text += "%s:%s\n" % (airport, country)
+def get_airport_country(airport_key):
+    """Fetch airport info from www.world-airport-codes.com."""
+    req = Request(compose_request(airport_key), headers={'User-Agent' : "kiwi project"})
+    response = send_request(req)
+    country_code = parse_country_code(response)
+    if country_code:
+        with lock:
+            dictionary_of_airports[airport_key] = country_code
+    else:
+        print("Couldn't find country for '%s' airport" % airport_key)
 
-    with open("airport_names.txt", "w") as airports_file:
-        airports_file.write(airport_list_text)
 
+def send_request(req):
+    """Function tries to send request 3 times before it moves on"""
+    tries = 0
+    while tries < 5:
+        try:
+            response = urlopen(req).read()
+            break
+        except URLError as error:
+            print("Error during %s try: %s" % (tries, error))
+            tries += 1
+            response = ""
+            sleep(2)
+    return response
+
+
+def compose_request(airport):
+    """compose string for search request"""
+    if len(airport) != 3:
+        raise Exception("Airport key '%s' is not valid" % airport)
+    request = "%ssearch/?s=%s" % (ADDRESS, airport.lower())
+    return request
+
+
+def parse_country_code(response):
+    """returns code of country"""
+    soup = BeautifulSoup(response, "html.parser")
+    result = str(soup.find_all("div", "header clearfix")[0])
+    soup = BeautifulSoup(result, "html.parser")
+    result = str(soup.find("p").contents[0][1:-1])
+    try:
+        code = COUNTRY_RE.match(result).group(1)
+    except AttributeError:
+        code = None
+    return code
 
 lock = RLock()
 dictionary_of_airports = {}
-# if __name__ == "__main__":
-#     main()
 
-main()
+if __name__ == "__main__":
+    main(100)
+
+# with open("timing.txt", "w") as timing_file:
+    # for i in range(5, 101, 5):
+    #     start = time()
+    #     for _ in range(10):
+    #         main(i)
+    #     timing_file.write("threads: %s, time: %s\n" % (i, time() - start))
